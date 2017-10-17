124e0a701a9ab4899dfa58a2b763b5851b0e41aa
burgersmoke <burgersmoke@gmail.com>
2017-10-17 09:32:22 -0600

    Updated trainer to work with NLTK 3 properly.

--- nltk_trainer/tagging/training.py
+++ nltk_trainer/tagging/training.py
@@ -1,13 +1,18 @@
 from nltk.tag import brill
+from nltk.tag.brill_trainer import BrillTaggerTrainer
+from nltk.tag.brill import Word, Pos
+
+from nltk.tbl import Feature, Template
 
 def train_brill_tagger(initial_tagger, train_sents, end, trace=0, **kwargs):
 	bounds = [(1, end)]
 	
-	templates = [
-		brill.SymmetricProximateTokensTemplate(brill.ProximateTagsRule, *bounds),
-		brill.SymmetricProximateTokensTemplate(brill.ProximateWordsRule, *bounds),
-	]
+	# call this to fetch templates directly
+	# NOTE : This is the comment from the method below:
+	#### Return 37 templates taken from the postagging task of the
+    #### fntbl distribution http://www.cs.jhu.edu/~rflorian/fntbl/
+	templates = brill.fntbl37()
 	
-	trainer = brill.FastBrillTaggerTrainer(initial_tagger, templates,
+	trainer = BrillTaggerTrainer(initial_tagger, templates,
 		deterministic=True, trace=trace)
 	return trainer.train(train_sents, **kwargs)
